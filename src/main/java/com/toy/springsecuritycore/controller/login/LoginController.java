bcfbb80cc94592291fecd6f5e6256f4d446508c3
Haril <songkg7@gmail.com>
2021-10-06 22:22:19 +0900

    feat: Logout 처리

--- src/main/java/com/toy/springsecuritycore/controller/login/LoginController.java
+++ src/main/java/com/toy/springsecuritycore/controller/login/LoginController.java
@@ -1,5 +1,10 @@
 package com.toy.springsecuritycore.controller.login;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -11,4 +16,12 @@ public class LoginController {
         return "/login";
     }
 
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+        return "redirect:/login";
+    }
 }
